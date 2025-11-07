40227a47a11dd95d389d9e4b16697d2a4a3256a9
Prisha <p.garg1606@gmail.com>
2025-11-07 18:23:51 -0500

    added code for the side bar

---
+++ script.js
@@ -0,0 +1,7 @@
+function showPage(pageID) {
+    document.querySelectorAll('.page').forEach(c => c.classList.add('hidden'));
+
+    const page = document.getElementById(pageID);
+    page.classList.remove('hidden');
+    window.scrollTo({ top: 0, behavior: "auto" });
+}

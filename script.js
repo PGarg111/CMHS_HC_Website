da0606b9184b454774f6a49531a0d581c4c372b7
Prisha <p.garg1606@gmail.com>
2025-12-05 22:53:33 -0500

    Added cards to the projects

--- script.js
+++ script.js
@@ -28,9 +28,11 @@ function toggleTheme() {
     document.body.classList.toggle('dark-mode');
 }
 
-const track = document.querySelector('.carousel-track');
 
 track.innerHTML += track.innerHTML;
 
 track.addEventListener('mouseenter', () => track.style.animationPlayState = 'paused');
 track.addEventListener('mouseleave', () => track.style.animationPlayState = 'running');
+
+const track = document.getElementById('track');
+    track.innerHTML += track.innerHTML;
